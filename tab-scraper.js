6e427f8e99856e09a7eb84148afd653aabda2e3b
Brian Schott <brianjschott@gmail.com>
2021-08-28 14:01:05 -0400

    tab-scraper gets the tabs, now have to add to the frontend

--- tab-scraper.js
+++ tab-scraper.js
@@ -1,13 +1,14 @@
-const request = require('request');
 const qs = require('querystring');
-const h2p = require('html2plaintext')
 const puppeteer = require('puppeteer')
-
+const fs = require('fs')
+const path = require('path')
 const baseSearchURL = "https://www.ultimate-guitar.com/search.php?"
 
-getResultsPageFromQuery("Madonna", "Into the Groove", "chords")
+takeScreenshotsOfTab("The Cure", "Friday I'm In Love", "chords")
 
-//cuts out any text following an open parentheses or dash
+
+//cuts out any text following an open parentheses or dash, and lowercases it
+//this is so the artist name can be fed into the URL for tabs
 function shortenTrackName(trackName, charactersToCut = ["(", "-"]) {
 
     let trackNameShortened = trackName
@@ -18,86 +19,108 @@ function shortenTrackName(trackName, charactersToCut = ["(", "-"]) {
     }
     return trackNameShortened.toLowerCase()
 }
-
-//returns an array of URLs containing the results from the results page
-//@param: query parameters, type is either "Chords" or "Tab"
-//query parameters include
+//gets type code from the type string
 //title will have song name and artist
 //type will be 200 for tabs, 300 for chords
-async function getResultsPageFromQuery(artistSearchQuery, trackNameSearchQuery, type) {
-    let typeCode = 0;
-    switch (type.toLowerCase()) {
+function getTypeCode(typeString) {
+    
+    switch (typeString.toLowerCase()) {
         case "tab": 
-            typeCode = 200
+            return 200
             break;
         case "chords":
-            typeCode = 300
+            return 300
             break;
         default: 
             return null;
     }
 
-    let url = baseSearchURL + "title=" + qs.escape(trackNameSearchQuery) + "&type=" + typeCode;
-    console.log(url)
-    let browser
-    try {
-        if (browser && browser.process() != null) browser.process().kill('SIGINT');
-    browser = await puppeteer.launch({ headless: true})
-    console.log("puppeteer launched")
-    const page = await browser.newPage()
-    console.log("Page opened")
-    await page.goto(url)
-    console.log("Visited URL:" + url)
-    await page.waitForSelector('a')
-    //waits for a tags to be rendered, then gets all of their href attributes
-    const urls = await page.$$eval('a', anchors => {
-        return anchors.map(anchor => 
-            { 
-                return anchor.getAttribute("href") 
-            }
-        )
-    })
+}
 
-    const baseSearchResultsUrl = "https://tabs.ultimate-guitar.com/tab/"
+function getSearchUrlFromQuery(artistSearchQuery, trackNameSearchQuery, type) {
+    return baseSearchURL + "title=" + qs.escape(trackNameSearchQuery) + "&type=" + getTypeCode(type);
+}
 
+//filters urls to specifically 
+//returns filtered array
+function filterURLs(urls, artistSearchQuery) {
     //gets only tabs that come from the tabs list, then further filters the list for only the ones in the sought artist
-    //also replaces spaces with dashes to work in the URL of the tab
+        //also replaces spaces with dashes to work in the URL of the tab
+        
+    const baseSearchResultsUrl = "https://tabs.ultimate-guitar.com/tab/"
     urlsFilteredTabs = urls.filter((x) => {return x.indexOf(baseSearchResultsUrl) > -1})
     urlsFilteredArtist = urlsFilteredTabs.filter((x) => {return x.indexOf(shortenTrackName(artistSearchQuery).replace(/ /g, "-")) > -1})
     if (urlsFilteredArtist == undefined) return false;
+    console.log("Filtered URLS" + urlsFilteredArtist)
+    return urlsFilteredArtist
+}
 
-    //go to the printable page
-    console.log("Going to tab page: " + urlsFilteredArtist[0])
-    await page.goto(urlsFilteredArtist[0])
-    console.log("Awaiting anchor element with the correct link...")
-    await page.waitForSelector('a[href*="/tab/print?flats"')
-    await page.screenshot({path: 'testscreenshot.png'}) //put in to diagnose "undefined" error for browser.pages()
-    //click button to download pdf (find button first)
-    const printURL = await page.$('a[href*="/tab/print?flats"')
-    await page.waitForTimeout(1000) //waits a second as it seems Print button doesn't load in immediately
+async function takeScreenshotsOfTab(artistSearchQuery, trackNameSearchQuery, type) {
+    //clear files
+    deleteFilesInDirectory('./tabs') //clears directory of previous tabs
+    let browser
+    //get search results list (this is where I should be checking ratings)
+    try {
+        if (browser && browser.process() != null) browser.process().kill('SIGINT'); //kills existing processes (not sure if it works)
+        browser = await puppeteer.launch({ headless: true})
+        const page = await browser.newPage()
+        await page.goto(getSearchUrlFromQuery(artistSearchQuery, trackNameSearchQuery, type))
+        await page.waitForSelector('a')
 
-    await printURL.click({
-        delay: 500
-    })
-    console.log("printURL has been clicked")
-    const newPagePromise = new Promise(res => browser.on('targetcreated', res))
+        //urls is an array of tabs with their href attributes
+        const urls = await page.$$eval('a', anchors => {
+            return anchors.map(anchor => 
+                { 
+                    return anchor.getAttribute("href") 
+                }
+            )
+        })
 
-    console.log("About to resolve promise...")
-    await newPagePromise
-    console.log("Promise resolved, now getting popup")
-    const pages = await browser.pages()
-    console.log("This should be 2: " + pages.length)
-    popup = pages[pages.length-1]
-    await popup.setViewport({
-        width: 1000,
-        height: 1000
-    })
-    console.log("Now waiting for the DIV with that class to appear")
-    await popup.waitForSelector("div.page-container")
-    console.log("Selector is there, about to iterate")
+        filteredUrls = filterURLs(urls, artistSearchQuery)
+        //count ratings and get the URL with the highest rating (should be the most full stars and the highest count of ratings, with the former having precedence)
+        //go to the printable page
+        console.log("Going to tab page: " + filteredUrls[0])
+        await page.goto(filteredUrls[0])
+        console.log("Awaiting anchor element with the correct link...")
+        await page.waitForSelector('a[href*="/tab/print?flats"]')
+        //click button to download pdf (find button first)
+        const printURL = await page.$('a[href*="/tab/print?flats"]')
+        await page.waitForTimeout(5000); //waits a second as it seems Print button doesn't load in immediately
 
-    const tabContainers = await popup.$$('div.page-container')
-    await captureScreenshotsOfElements(tabContainers, popup)
+        await printURL.click({
+                button: "middle",
+                clickCount: 2 //I don't know why, but two clicks seems to help here!
+            });
+
+
+        console.log("printURL has been clicked")
+        const newPagePromise = new Promise(res => browser.on('targetcreated', res))
+
+        console.log("About to resolve promise... (code hangs here frequently)")
+        await newPagePromise
+        console.log("Promise resolved, now getting popup")
+        const pages = await browser.pages()
+        popup = pages[pages.length-1]
+        await popup.setViewport({
+            width: 3000,
+            height: 1000,
+            deviceScaleFactor: 2
+        })
+        console.log("Now waiting for the DIV with the lyrics class to appear")
+        await popup.waitForSelector("div.page-container")
+        console.log("Selector is there, about to iterate")
+
+        const tabContainers = await popup.$$('div.page-container')
+        console.log("There is this many tab containers: " + tabContainers.length)
+        await popup.waitForTimeout(2000)
+        //await captureScreenshotsOfElements(tabContainers, popup)
+        let i = 0;
+        for (let element of tabContainers) {
+            await element.screenshot({ path: `tabs/${i}.png` });
+            i += 1;
+        }
+        //note the above i value because we need to know how many screenshots were just taken
+        //alternative I can empty out the tabs/ folder every time
     }
     catch (e) {
         console.log(e)
@@ -108,11 +131,13 @@ async function getResultsPageFromQuery(artistSearchQuery, trackNameSearchQuery, 
     }
 }
 
-const captureScreenshotsOfElements = async (elements) => {
-    let i = 0;
-    for (const element of elements) {
-      await element.screenshot({ path: `tmp/${i}.png` });
-      i += 1;
-    }
-  };
-
+function deleteFilesInDirectory(dirPath) {
+    fs.readdir(dirPath, (err, files) => {
+        if (err) console.log(err);
+        for (const file of files) {
+            fs.unlink(path.join(dirPath, file), err => {
+                if (err) console.log(err);
+            });
+        }
+    });
+}
